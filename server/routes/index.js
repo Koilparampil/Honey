cf473e800e8f51392ac0ee2645dcca97fbecf28c
KreshsDesktop <ckresh99@gmail.com>
2022-04-05 14:36:42 -0400

    now calling if User Exists

--- server/routes/index.js
+++ server/routes/index.js
@@ -58,9 +58,9 @@ router.route('/login').post(async(req,res) => {
     try{
         const {email,password} =req.body
         logInUser(req, res).then(async (user) => {
-            console.log(user.password)
+            console.log(user?.password)
             console.log(password)
-            const correctPassword = await bcrypt.compare(password, user.password) 
+            const correctPassword = await bcrypt.compare(password, user?.password) 
             if (user && correctPassword) {
                 const token =jwt.sign(user.toJSON(),email, {
                     expiresIn:60*24
